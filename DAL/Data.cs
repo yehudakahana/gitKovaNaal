25b9020a8ad7a23cf1da726453a0c4377860ed55
Matan <jerbimatan@gmail.com>
2024-08-05 10:02:14 +0300

    DAL folder

---
+++ DAL/Data.cs
@@ -0,0 +1,30 @@
+﻿namespace gitKovaNaal.DAL
+{
+    public class Data
+    {
+        private readonly string ConnectionString = "SERVER = DESKTOP-FR8PH7L\\MATAN_SQL;initial catalog=MyLibrary; user id=SA; " +
+            "password=1234;TrustServerCertificate=Yes";
+        private static Data _data;
+        private DataLayer DataLayer;
+        private Data()
+        {
+            DataLayer = new DataLayer(ConnectionString);
+        }
+        public static DataLayer Get
+        {
+            get
+            {
+                if (_data == null)
+                {
+                    _data = new Data();
+                }
+                return _data.DataLayer;
+            }
+        }
+        public void Add()
+        {
+
+
+        }
+    }
+}
